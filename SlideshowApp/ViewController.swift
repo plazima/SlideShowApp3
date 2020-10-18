7821af534b75f6634cac9a5116a6a0ad9aa10c99
takumi.hatae <plazima0131@softbank.ne.jp>
2020-10-18 11:46:53 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by plazima on 2020/10/18.
+//  Copyright © 2020 takumi.hatae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
